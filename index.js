dd92328b6019c1bdd503ff9e15e88c7faeb1dbb3
Nicolás Chiesa <nicochiesa01@gmail.com>
2026-08-25 07:36:04 -0300

    uibuild

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ const Person = require("./models/person")
 morgan.token('body', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.get('/info', (request, response) => {
   const date = new Date()
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
@@ -26,22 +36,21 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(note => {
     response.json(note)
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(deletedPerson => {
       deletedPerson
         ? response.status(204).end()
         : response.status(404).end()
     })
-    .catch(error => {
-      response.status(400).json({ error: 'malformatted id' })
-    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -52,6 +61,21 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndUpdate(
+    request.params.id,
+    request.body
+  )
+    .then(updatedPerson => {
+      updatedPerson
+        ? response.json(updatedPerson)
+        : response.status(404).end()
+    })
+    .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
