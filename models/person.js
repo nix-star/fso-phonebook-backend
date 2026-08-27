052b7aeec4fb22e2662fd22996416cf0e830f8ff
Nicolás Chiesa <nicochiesa01@gmail.com>
2026-08-27 19:23:14 -0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -18,7 +18,7 @@ mongoose.connect(url, { family: 4 })
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 5,
+    minLength: 3,
     required: true
   },
   number: String
